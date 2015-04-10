0edd72394218df34e0b8cac6305e0fe31caf2580
unknown <johnnybgoode@rogers.com>
2015-04-10 13:37:15 -0400

    did some more stuff, gotta test something

--- matters.cpp
+++ matters.cpp
@@ -8,5 +8,18 @@
 
 //#include "Headers.hpp"
 //#include "Source.cpp"
+#include "matters.hpp"
 
 
+
+Element::Element(element type)
+{	Atomic_number = Atomic_number_of(type);
+}
+
+Element::Element(atomic_number number)
+{	Atomic_number = number;
+}
+
+Element::~Element()
+{
+}
