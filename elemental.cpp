b0f92174b82509ba29e4d781f4af877abe9c02c3
unknown <johnnybgoode@rogers.com>
2015-04-10 14:00:45 -0400

    okay, codeblocks is now agreeing to play nice

---
+++ elemental.cpp
@@ -0,0 +1,24 @@
+// elemental.cpp ///////////////////////////////////////////////////////////////
+// some horrible long functions from the matters ///////////////////////////////
+// module that I wanted to put elsewhere ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//#include <std_files>
+//#include "Headers.h"
+
+//#include "Source.c"
+
+//#include "Headers.hpp"
+//#include "Source.cpp"
+#include "matters.hpp"
+
+std::string element_name(element input)
+{	std::string output = "foo";
+	return output;
+}
+
+atomic_number Atomic_number_of(element input)
+{	return 1;
+	// everything is hydrogen, the physicists are happy
+	
+}
+
